a165398da04a37299afda7df1803cda874f8c01d
Jorge Vega <vegajor1112@gmail.com>
2022-03-19 16:38:25 -0300

    get countries route

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,36 +1,40 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Country", {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
+  sequelize.define(
+    "Country",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      bandera: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continente: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+      },
+      area: {
+        type: DataTypes.STRING,
+      },
+      poblacion: {
+        type: DataTypes.INTEGER,
+      },
     },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    bandera: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    continente: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-    },
-    area: {
-      type: DataTypes.STRING,
-    },
-    poblacion: {
-      type: DataTypes.INTEGER,
-    },
-  });
+    { timestamps: false }
+  );
 };
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,14 +1,47 @@
 const { urlencoded } = require("body-parser");
+const axios = require("axios");
+const { Country } = require("../db");
 
-const countries = (req, res) => {
-  if (req.url.includes("?")) return res.send("Ruta con query");
+const countries = async (req, res) => {
+  let countriesData;
 
-  console.log(req.url);
-  res.send("Ruta de GET countries");
+  console.log("Buscamos primero en la base de datos");
+  try {
+    countriesData = await Country.findAll({ raw: true });
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (countriesData.length === 0) {
+    console.log("Estoy buscando en la API porque la base de datos está vacía");
+    countriesData = await axios("https://restcountries.com/v3.1/all");
+    countriesData = countriesData.data;
+
+    countriesData = countriesData.map((country) => {
+      let capital;
+
+      if (!country.capital) capital = "No posee";
+      else capital = country.capital[0];
+
+      return {
+        id: country.cca3,
+        nombre: country.name.common,
+        bandera: country.flags.png,
+        continente: country.region,
+        capital: capital,
+        subregion: country.subregion,
+        area: country.area,
+        poblacion: country.population,
+      };
+    });
+    await Country.bulkCreate(countriesData);
+  }
+
+  res.send(countriesData);
 };
 
-const countriesId = (req, res) => {
-  res.send(`Ruta que recibe id: ${req.params.idCountry}`);
+const countriesId = async (req, res) => {
+  res.send("Ruta de GET countries");
 };
 
 module.exports = { countries, countriesId };
